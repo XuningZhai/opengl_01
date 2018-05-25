6b411c44fa759a105ffec2716be2cdc81f2c0b65
aipu <aipu@aiputekimacbook-pro.local>
2018-05-25 17:00:47 +0800

    Initial Commit

---
+++ opengl_01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  opengl_01
+//
+//  Created by aipu on 2018/5/25.
+//  Copyright © 2018年 aipu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ opengl_01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  opengl_01
+//
+//  Created by aipu on 2018/5/25.
+//  Copyright © 2018年 aipu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
